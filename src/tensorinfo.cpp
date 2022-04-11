5fac7d45fb428f57604c62f2fbdfe2d0a1ca4f68
atobisze <adrian.tobiszewski@intel.com>
2022-04-11 14:51:49 +0200

    Add KFS predict request validation
    JIRA:CVS-83547

--- src/tensorinfo.cpp
+++ src/tensorinfo.cpp
@@ -326,6 +326,21 @@ std::string TensorInfo::tensorShapeToString(const tensorflow::TensorShapeProto& 
     return oss.str();
 }
 
+std::string TensorInfo::tensorShapeToString(const google::protobuf::RepeatedField<int64_t>& shape) {
+    std::ostringstream oss;
+    oss << "(";
+    size_t i = 0;
+    if (shape.size() > 0) {
+        for (; i < shape.size() - 1; i++) {
+            oss << shape[i] << ",";
+        }
+        oss << shape[i];
+    }
+    oss << ")";
+
+    return oss.str();
+}
+
 std::shared_ptr<TensorInfo> TensorInfo::getUnspecifiedTensorInfo() {
     return std::make_shared<TensorInfo>("", Precision::UNDEFINED, Shape{});
 }
